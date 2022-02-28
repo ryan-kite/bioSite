add1960230dbacf0488ed1b30cb0d367086f67f7
ryan-kite <ryankiter@gmail.com>
2022-02-28 01:38:00 -0800

    added mobile menu

--- js/cd.js
+++ js/cd.js
@@ -1,25 +1,41 @@
-let stateOpen = false
-function toggleNav() {
+let stateOpen = true;
+document.getElementById("mobile-menu").style.display = "none";
 
-  if (stateOpen == true) {
-    console.log("open")
+
+function toggleNav() {  
+
+  try {
+    stateOpen =  window.localStorage.getItem("state")
+    console.log("state (read) from storage: ", stateOpen)
+    
+  } catch (error) {
+    console.log("state (read) from storage: ", error)
+    stateOpen = true;
+  }
+
+
+  if (stateOpen === true || stateOpen === 'true')  {
     /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
     document.getElementById("side-bar").style.marginLeft = "0";
     document.getElementById("main").style.marginLeft = "225px";
     document.getElementById("masthead").style.marginLeft = "225px";
-    stateOpen = false
-
+    document.getElementById("mobile-menu").style.display = "none";
+    document.getElementById("main-menu").style.display = "block";
+    stateOpen = false;
+    window.localStorage.setItem("state", stateOpen)
+    console.log("state if (set) ", stateOpen)
   } 
   else {
-    console.log("close")
     /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
     document.getElementById("side-bar").style.marginLeft = "-185px";
     document.getElementById("main").style.marginLeft = "40px";
-    document.getElementById("masthead").style.marginLeft = "40px";
-    stateOpen = true
+    document.getElementById("masthead").style.marginLeft = "40px";    
+    document.getElementById("mobile-menu").style.display = "block";
+    document.getElementById("main-menu").style.display = "none";
+    stateOpen = true;
+    window.localStorage.setItem("state", stateOpen)
+    console.log("state else (set) ", stateOpen)
   }
-    
-  
 }
 
 function signUp() {
@@ -33,9 +49,6 @@ function signUp() {
   }
   
 }
-
-
-
 
 
 console.log("loaded cd.js")
